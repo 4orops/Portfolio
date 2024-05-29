ecb3ae6213a2f9c31e0ab55acce89e0f3aaceed0
4orops <wabile4@gmail.com>
2024-05-29 12:52:12 +0200

    Adding more skillset

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -6,11 +6,19 @@ import {
   DiNodejs,
   DiMongodb,
   DiGit,
+  DiHtml5,
+  DiCss3
 } from "react-icons/di";
 
 function Techstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiHtml5 />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiCss3 />
+      </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiJavascript1 />
       </Col>
